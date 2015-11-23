ec6377fd7359c57c4c45c977c9a9c4c50d341e3c
CQ <260732891@qq.com>
2015-11-23 17:38:16 +0800

    new

---
+++ aaa/aaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaa
+//
+//  Created by 阿城 on 15/11/23.
+//  Copyright © 2015年 阿城. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
